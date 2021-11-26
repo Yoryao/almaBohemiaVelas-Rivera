8989e5ed0eb130d7043b5f87272aea24ff41053c
yoryao <jorgejoaquinrivera@gmail.com>
2021-11-26 00:11:06 -0300

    app creation and initial changes on main page

---
+++ LOG.md
@@ -0,0 +1,13 @@
+# Proyecto Curso React - Coder - NOVIEMBRE 2021
+
+Creación del proyecto final (e-commerce) "Alma Bohemia Velas"
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### Segunda Clase. 25/11/2021
+
+Creación de la app. Instalación de paquetas y entorno de desarrollo. 
+Estilo incial. Icono en barra de titulo y logo en pagina principal.
+Agregado de Font from google fonts, atravez de <link> en index.html
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './candletitle.svg';
 import './App.css';
 
 function App() {
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p id="mainTitle">
+        Alma Bohemia Velas
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ingresar
         </a>
       </header>
     </div>
