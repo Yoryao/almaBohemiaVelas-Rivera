1c2eece53448409e03a61c03c6b161f3610b968c
yoryao <jorgejoaquinrivera@gmail.com>
2021-12-20 15:45:37 -0300

    desafio detail completo

--- src/components/main/ItemDetail.js
+++ src/components/main/ItemDetail.js
@@ -1,26 +1,27 @@
-import React, { useState , useEffect } from 'react'
-import ItemCount from '../section/ItemCount';
+import React, { useState, useEffect } from "react";
+import ItemCount from "../section/ItemCount";
 
 export const ItemDetail = (props) => {
-  
-    console.log("Props dentro de ItemDetail")
-    console.log(props)
+  console.log("Props dentro de ItemDetail");
+  console.log(props);
 
+  // console.log("detail dentro de ItemDetail")
+  // console.log(detail)
 
-    // console.log("detail dentro de ItemDetail")
-    // console.log(detail)
-
-    return (
-        <div>
-            <h3>Id:{props.detail.id}</h3>
-            <h3>Nombre: {props.detail.nombre}</h3>
-            <h3>Frangacia: {props.detail.description}</h3>
-            <h3>Precio: ${props.detail.price}</h3>
-            <ItemCount stock={7} initial={5}
-            // onAdd={onAdd} 
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <img alt="logo" src={(props.detail.img)}  />
+      <h3>Id:{props.detail.id}</h3>
+      <h3>Nombre: {props.detail.nombre}</h3>
+      <h3>Frangacia: {props.detail.description}</h3>
+      <h3>Precio: ${props.detail.price}</h3>
+      <ItemCount
+        stock={7}
+        initial={12}
+        // onAdd={onAdd}
+      />
+    </div>
+  );
+};
 
 export default ItemDetail;
--- src/components/main/ItemDetailContainer.js
+++ src/components/main/ItemDetailContainer.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from "react";
 import ItemDetail from "./ItemDetail";
 
-const inicial = { id: 1, nombre: "difusor", description: "Coco", price: 190 }
-
-
+const inicial = { id: 1, nombre: "difusor", description: "Coco", price: 190, img: "./vela.jpg" };
 
 export const ItemDetailContainer = () => {
-
-
   const [detail, setDetail] = useState({});
 
-  useEffect(() => {
-    const promesa = new Promise((res, rej) => {
-      setTimeout(() => {
-        res(inicial);
-      }, 2000);
-    }, []);
-
-    promesa
-      .then((item) => {
-        console.log("Funca el detalle");
-        setDetail(item);
-      })
-      .catch(() => {
-        console.log("no funca el Detalle");
-      });
-  });
+  const getItem = () => {
  
+      const promesa = new Promise((res, rej) => {
+        setTimeout(() => {
+          res(inicial);
+        }, 2000);
+      }, []);
+
+      promesa
+        .then((item) => {
+          console.log("Funca el detalle");
+          setDetail(item);
+        })
+        .catch(() => {
+          console.log("no funca el Detalle");
+        });
+    
+  };
+
+useEffect(() => {
+  getItem();
+})
+
+
+
+
+
   return (
     <div>
       <h1> ACA VA UN ITEM DETAIL </h1>
