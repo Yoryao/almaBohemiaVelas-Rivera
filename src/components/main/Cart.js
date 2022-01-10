54e8baf31e1822f2bdd5cbf35d2eca6a1daadc77
yoryao <jorgejoaquinrivera@gmail.com>
2022-01-10 00:21:42 -0300

    falta la funcion isincart

--- src/components/main/Cart.js
+++ src/components/main/Cart.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { isElementOfType } from "react-dom/cjs/react-dom-test-utils.production.min";
 import { Link } from "react-router-dom";
 import { MyHook } from "../../../src/context/CartContext";
 
 function Cart() {
-  const { carrito, precioTotal, clear, borrarDelCarrito } = MyHook();
+  const { carrito, clear, borrarDelCarrito, valorCarrito, setValorCarrito } = MyHook();
   console.log(carrito);
+//   valorCarrito = 0;
+
+const sumall = carrito.map(item => item.precioParcial).reduce((prev, curr) => prev + curr, 0);
+console.log(parseInt(sumall))
+setValorCarrito(sumall);
+
+
+
   return (
     <>
           <h3>Listado de productos en carrito</h3>
       {carrito.map((element) => {
+
+    let parcialProducto = element.cantidad * element.precio;
+    element.precioParcial = parcialProducto;
+
         return (
           <div>
            
             <span>Nombre: {element.nombre} </span>
-            <span>Precio: ${element.precio}</span>
+            <span>Precio: ${element.precio} </span>
+            <span>Cantidad: {element.cantidad} </span>
+            <span>Precio Parcial: ${parcialProducto} </span>
+
             <span>id : {element.id}</span>
             <button onClick={() => {borrarDelCarrito(element.id)}}>
                 Del</button>
           </div>
         );
     })}
-    <span>Precio Total del carrito: ${precioTotal}</span>
+    <span>Precio Total del carrito: ${valorCarrito}</span>
       <Link to={`/productos`}>
         <button variant="primary">Seguir Comprando</button>
       </Link>
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -13,77 +13,69 @@ const MyProvider = ({ children }) => {
   //cantidad de productos
   const [cantidad, setCantidad] = useState(0);
   //cantidad de items
-  const [cantidadItems, setCantidadItems] = useState(0)
+  const [cantidadItems, setCantidadItems] = useState(0);
 
-  const [precioParcial, setPrecioParcial] = useState(0)
-  //precio del total de productos
-  const [precioTotal, setPrecioTotal] = useState(0);
-  // array de objetos con los productos.
+  const [valorCarrito, setValorCarrito] = useState(0);
+
   const [carrito, setCarrito] = useState([]);
 
   //fx para agregar producto y cantidad al carrito
   const agregarAlCarrito = (producto, cantidad, id) => {
 
+isInCarrito(id);
 
 
 
 
 
+    setCantidadItems(cantidadItems + 1);
 
-
-    setCantidadItems(cantidadItems + 1);    
-    
-    const copia_producto = {...producto}
-    copia_producto.cantidad = cantidad
-
+    const copia_producto = { ...producto };
+    copia_producto.cantidad = cantidad;
 
     ////TODO: NO ESTA SUMANDO EL ULTIMO PRECIO A LA CUENTA TOTAL
 
-   setPrecioParcial(copia_producto.precio * cantidad)
-   setPrecioTotal(precioTotal + precioParcial)
-
-   console.log(precioParcial)
-   console.log(precioTotal)
-
-
-  const copia = [...carrito]
-        copia.push(copia_producto)
-        setCarrito(copia) 
-        console.log(carrito)
-
+    const copia = [...carrito];
+    copia.push(copia_producto);
+    setCarrito(copia);
+    console.log(carrito);
   };
 
   //fx para borrar un producto del carrito
-  const borrarDelCarrito = ( id ) => {
+  const borrarDelCarrito = (id) => {
+    const newCarrito = carrito.filter((item) => item.id !== id);
 
-    console.log("borrando elemento con id_ " + id)
-
-
+    setCarrito(newCarrito);
+    setCantidadItems(cantidadItems - 1);
   };
 
   //borrar todo el carrito
   const clear = () => {
     setCarrito([]);
-    setPrecioTotal(0);
-    setPrecioParcial(0);
+    setValorCarrito(0);
+    setCantidadItems(0);
   };
 
   //verificacion si el producto esta en el carrito
   const isInCarrito = (id) => {
-
-
-
-
-
-
+    const idInCarrito = carrito.filter((item) => item.id == id);  
+      
+    if (idInCarrito == [] ) { console.log("No Esta en el carrito") }
+    else { console.log("esta en el carrito") }
+ 
 
   };
 
   const valorDelContexto = {
-    cantidad,    setCantidad,
-    carrito,     setCarrito,
-    precioTotal, setPrecioTotal,
-    cantidadItems, setCantidadItems,
+    cantidad,
+    setCantidad,
+    carrito,
+    setCarrito,
+    valorCarrito,
+    setValorCarrito,
+
+    cantidadItems,
+    setCantidadItems,
     agregarAlCarrito,
     borrarDelCarrito,
     clear,
