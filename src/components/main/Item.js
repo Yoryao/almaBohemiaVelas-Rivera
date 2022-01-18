abe58e52db2eace28d308521f9d0a6f7ba686d53
yoryao <jorgejoaquinrivera@gmail.com>
2022-01-18 15:07:37 -0300

    working on cards spacing

--- src/components/main/Item.js
+++ src/components/main/Item.js
@@ -1,21 +1,24 @@
 import React from "react";
-import "./item.css"
+import "./item.css";
 import { Link } from "react-router-dom";
 
-
 export const Item = (props) => {
-
   return (
     <div className="card" id="divProductos">
       <img variant="top" src={props.img} alt="" />
-      <div>
+      <div id="detailInfo">
         <h2>Nombre: {props.nombre}</h2>
         <h4>Precio: $ {props.precio}</h4>
-        <Link to={`/item/${props.id}`}><button variant="primary">Ver Detalle</button>
+      </div>
+      <div id="detailDiv">
+        <Link to={`/item/${props.id}`}>
+          <button id="detailButton"  variant="primary">
+            Ver Detalle
+          </button>
         </Link>
       </div>
     </div>
   );
-  }
+};
 
 export default Item;
